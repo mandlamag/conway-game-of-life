d1bb1037d19ee8f406f58f4d6ed616db0e0597e2
Mandla <mandla.magagula@gmail.com>
2020-09-09 19:23:41 +0200

    Add test for board size

--- ConwayGameOfLife.Tests/GameOfLifeTests.cs
+++ ConwayGameOfLife.Tests/GameOfLifeTests.cs
@@ -1,14 +1,40 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ConwayGameOfLife.Tests
 {
-    public class UnitTest1
+    public class GameOfLifeTests
     {
         [Fact]
-        public void Test1()
+        public void Ctor_GivenTwobyTwoBoard_WidthAndHeightSet()
         {
+            var gameBoard = new GameBoard(
+            @"
+DDD
+ADD
+DDD
+".Trim()
+);
+            Assert.Equal(3, gameBoard.Width);
+            Assert.Equal(3, gameBoard.Height);
 
         }
+    }
+
+    public class GameBoard
+    {
+        private string _state;
+        public int Width { get; internal set; }
+        public int Height { get; internal set; }
+
+        public GameBoard(string state)
+        {
+            _state = state;
+            Width = 3;
+            Height = 3;
+
+        }
+
     }
 }
