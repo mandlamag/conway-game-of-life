51234881be033e3bd3b51c52f61d1b6a3d6c9468
Mandla <mandla.magagula@gmail.com>
2020-09-09 19:50:29 +0200

    Implment test case for configurable board size

--- ConwayGameOfLife.Tests/GameOfLifeTests.cs
+++ ConwayGameOfLife.Tests/GameOfLifeTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace ConwayGameOfLife.Tests
@@ -7,32 +8,48 @@ namespace ConwayGameOfLife.Tests
     public class GameOfLifeTests
     {
         [Fact]
-        public void Ctor_GivenTwobyTwoBoard_WidthAndHeightSet()
+        public void Ctor_GivenThreeByThreeBoard_WidthAndHeightSet()
         {
             var gameBoard = new GameBoard(
-            @"
-DDD
+        @"
 ADD
-DDD
-".Trim()
+ADD
+DDD".Trim()
 );
             Assert.Equal(3, gameBoard.Width);
             Assert.Equal(3, gameBoard.Height);
 
         }
+
+
+        [Fact]
+        public void Ctor_GivenFivebyFixBoard_WidthAndHeightSet()
+        {
+            var gameBoard = new GameBoard(
+        @"
+ADDDD
+ADDDD
+ADDDD
+AAADD
+DDDDD".Trim()
+);
+            Assert.Equal(5, gameBoard.Width);
+            Assert.Equal(5, gameBoard.Height);
+
+        }
     }
+
 
     public class GameBoard
     {
-        private string _state;
-        public int Width { get; internal set; }
-        public int Height { get; internal set; }
+        private List<string> _state;
+        public int Width { get { return _state.Max(x => x.Length); } }
+        public int Height { get { return _state.Count; } }
+
 
         public GameBoard(string state)
         {
-            _state = state;
-            Width = 3;
-            Height = 3;
+            _state = state.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
 
         }
 
